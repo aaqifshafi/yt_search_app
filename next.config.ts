f3278b3e3b89f556d74641fe61ff6698d9323708
Aaqif Shafi <63208372+aaqifshafi@users.noreply.github.com>
2024-12-13 00:49:43 +0530

    Update image configuration to use remote patterns for external domains

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["i.ytimg.com", "yt3.ggpht.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "yt3.ggpht.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
